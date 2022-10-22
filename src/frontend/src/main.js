f60ca54c9bce518c9158736fa54bb4fd9a9d3923
cyoq <di9akoz@gmail.com>
2022-10-22 15:38:22 +0300

    Trying to connect frontend with other parts

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
-import "./assets/main.css";
+import axios from "axios";
+import ElementPlus from "element-plus";
 
 const app = createApp(App);
 
+// axios.defaults.baseURL = import.meta.env.VUE_APP_API_URL;
+axios.defaults.baseURL = "http://localhost:9091";
+
+app.use(ElementPlus);
 app.use(router);
 
 app.mount("#app");
--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from "node:url";
 
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const config = dotenv.config({ path: "../.env" });
+dotenvExpand.expand(config);
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  // process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-});
+  });
+};

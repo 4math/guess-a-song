03d9dde764d00e237761bb6b2fe0b5a3d5b1d4e0
Maxim01U <maxim.uuuvvv@gmail.com>
2022-11-05 18:55:02 +0200

    initial homescreen

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import TodoComponent from "../components/Todo.vue";
 import LeaderBoardComponent from "../components/LeaderBoard.vue";
+import HomeScreen from "../components/HomeScreen.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: TodoComponent,
+      component: HomeScreen,
     },
     {
       path: "/leaderboard",
